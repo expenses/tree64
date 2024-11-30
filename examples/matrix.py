adef5584e1f3832fa2ada0981b81f0f075d4e59c
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-11-30 16:57:16 +0000

    matrix changes

--- examples/matrix.py
+++ examples/matrix.py
@@ -6,22 +6,22 @@ arr = np.zeros((5, dim, dim), dtype=np.uint8)
 
 ffmpeg = FfmpegOutput("out.avi", width=dim, height=dim, skip=40)
 
-arr[0,dim // 2, dim // 2] = 1
+arr[0, dim // 2, dim // 2] = 1
 
 rep(
     arr,
     Markov(
-        '''
+        """
         *2*,
         212,
         *2*=
         *2*,
         222,
         *2*
-        ''',
-   '101=121',
-        '100=121'
-        ),
+        """,
+        "101=121",
+        "100=121",
+    ),
     callback=lambda index: ffmpeg.write(arr[2]),
 )
-#save_as_voxels("out.vox", arr)
+save_as_voxels("out.vox", arr)
