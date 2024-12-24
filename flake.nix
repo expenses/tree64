34d8059a45d1de72bb7da75dd64d60a8b8e32908
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-12-24 08:50:22 +0000

    Start on voxviewer

--- flake.nix
+++ flake.nix
@@ -150,6 +150,24 @@
               openusd
             ];
           };
+        devShells.bevy = with pkgs;
+          mkShell rec {
+            nativeBuildInputs = [
+              pkg-config
+            ];
+            buildInputs = [
+              udev
+              alsa-lib
+              vulkan-loader
+              xorg.libX11
+              xorg.libXcursor
+              xorg.libXi
+              xorg.libXrandr # To use the x11 feature
+              libxkbcommon
+              wayland # To use the wayland feature
+            ];
+            LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+          };
       }
     );
 }
