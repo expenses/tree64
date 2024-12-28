7198236354304a236d8bb1be8431a1765f366c54
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-12-28 05:09:03 +0000

    Slowly getting there

--- voxviewer/src/main.rs
+++ voxviewer/src/main.rs
@@ -1,7 +1,7 @@
 use bevy::{
-    core_pipeline::core_3d::{graph::Core3d, Opaque3d, Opaque3dBinKey, CORE_3D_DEPTH_FORMAT},
-    ecs::query::WorldQuery,
+    core_pipeline::core_3d::{graph::Core3d, CORE_3D_DEPTH_FORMAT},
     ecs::{
+        query::QueryItem,
         query::ROQueryItem,
         system::{lifetimeless::SRes, SystemParamItem},
     },
@@ -11,13 +11,14 @@ use bevy::{
     render::primitives::Aabb,
     render::render_graph::{NodeRunError, RenderGraphContext, ViewNodeRunner},
     render::{
+        camera::ExtractedCamera,
         extract_component::{ExtractComponent, ExtractComponentPlugin},
-        primitives::*,
-        render_graph::{self, RenderGraph, RenderGraphApp, RenderLabel},
+        render_graph::{self, RenderGraphApp, RenderLabel},
         render_phase::*,
         render_resource::{binding_types::*, *},
         renderer::{RenderContext, RenderDevice},
-        view::{self, ExtractedView, RenderVisibleEntities, VisibilitySystems},
+        sync_world::MainEntity,
+        view::*,
         *,
     },
 };
@@ -60,6 +61,28 @@ fn setup(mut commands: Commands) {
     ));
 }
 
+fn prepare_voxel_render_pipelines(
+    mut commands: Commands,
+    pipeline_cache: Res<PipelineCache>,
+    mut pipelines: ResMut<SpecializedRenderPipelines<RenderingPipeline>>,
+    pipeline: Res<RenderingPipeline>,
+    views: Query<(Entity, &ExtractedView, &Msaa)>,
+) {
+    for (entity, view, msaa) in &views {
+        let view_key = MeshPipelineKey::from_msaa_samples(msaa.samples())
+            | MeshPipelineKey::from_hdr(view.hdr);
+
+        let pipeline_id = pipelines.specialize(&pipeline_cache, &pipeline, view_key);
+
+        commands
+            .entity(entity)
+            .insert(VoxelRenderPipeline(pipeline_id));
+    }
+}
+
+#[derive(Component)]
+struct VoxelRenderPipeline(CachedRenderPipelineId);
+
 #[derive(Debug, Hash, PartialEq, Eq, Clone, RenderLabel)]
 struct VoxelRendererLabel;
 
@@ -83,9 +106,17 @@ impl Plugin for VoxelRendererPlugin {
         let render_app = app.sub_app_mut(RenderApp);
 
         render_app
+            .init_resource::<DrawFunctions<VoxelBinnedPhaseItem>>()
+            .init_resource::<ViewBinnedRenderPhases<VoxelBinnedPhaseItem>>()
             .init_resource::<SpecializedRenderPipelines<RenderingPipeline>>()
-            .add_render_command::<Opaque3d, DrawVoxelCubesCommands>()
-            .add_systems(Render, queue_custom_phase_item.in_set(RenderSet::Queue));
+            .add_render_command::<VoxelBinnedPhaseItem, DrawVoxelCubesCommands>()
+            .add_systems(
+                Render,
+                (
+                    queue_custom_phase_item.in_set(RenderSet::Queue),
+                    prepare_voxel_render_pipelines.in_set(RenderSet::Prepare),
+                ),
+            );
         /*render_app.add_systems(
             Render,
             prepare_bind_groups.in_set(RenderSet::PrepareBindGroups),
@@ -96,8 +127,8 @@ impl Plugin for VoxelRendererPlugin {
         render_app.add_render_graph_edges(
             Core3d,
             (
-                bevy::core_pipeline::core_3d::graph::Node3d::MainOpaquePass,
                 VoxelRendererLabel,
+                bevy::core_pipeline::core_3d::graph::Node3d::MainOpaquePass,
             ),
         );
     }
@@ -461,26 +492,18 @@ impl FromWorld for VoxelRendererPipeline {
 /// A render-world system that enqueues the entity with custom rendering into
 /// the opaque render phases of each view.
 fn queue_custom_phase_item(
-    mut opaque_render_phases: ResMut<ViewBinnedRenderPhases<Opaque3d>>,
-    opaque_draw_functions: Res<DrawFunctions<Opaque3d>>,
-    pipeline: Res<RenderingPipeline>,
-    views: Query<(Entity, &RenderVisibleEntities, &ExtractedView, &Msaa)>,
-    pipeline_cache: Res<PipelineCache>,
-    mut specialized_render_pipelines: ResMut<SpecializedRenderPipelines<RenderingPipeline>>,
+    mut opaque_render_phases: ResMut<ViewBinnedRenderPhases<VoxelBinnedPhaseItem>>,
+    opaque_draw_functions: Res<DrawFunctions<VoxelBinnedPhaseItem>>,
+    views: Query<(Entity, &RenderVisibleEntities)>,
 ) {
     let draw_custom_phase_item = opaque_draw_functions.read().id::<DrawVoxelCubesCommands>();
 
     // Render phases are per-view, so we need to iterate over all views so that
     // the entity appears in them. (In this example, we have only one view, but
     // it's good practice to loop over all views anyway.)
-    for (view_entity, view_visible_entities, view, msaa) in &views {
-        let Some(opaque_phase) = opaque_render_phases.get_mut(&view_entity) else {
-            continue;
-        };
-
-        // Create the key based on the view. In this case we only care about MSAA and HDR
-        let view_key = MeshPipelineKey::from_msaa_samples(msaa.samples())
-            | MeshPipelineKey::from_hdr(view.hdr);
+    for (view_entity, view_visible_entities) in &views {
+        let opaque_phase = opaque_render_phases.entry(view_entity).or_default();
+        opaque_phase.clear();
 
         // Find all the custom rendered entities that are visible from this
         // view.
@@ -494,16 +517,8 @@ fn queue_custom_phase_item(
             // but you can use anything you like. Note that the asset ID need
             // not be the ID of a [`Mesh`].
             opaque_phase.add(
-                Opaque3dBinKey {
+                VoxelBinnedPhaseItemBinKey {
                     draw_function: draw_custom_phase_item,
-                    pipeline: specialized_render_pipelines.specialize(
-                        &pipeline_cache,
-                        &pipeline,
-                        view_key,
-                    ),
-                    asset_id: AssetId::<Mesh>::invalid().untyped(),
-                    material_bind_group_id: None,
-                    lightmap_image: None,
                 },
                 entity,
                 BinnedRenderPhaseType::NonMesh,
@@ -512,7 +527,7 @@ fn queue_custom_phase_item(
     }
 }
 
-type DrawVoxelCubesCommands = (SetItemPipeline, SetMeshViewBindGroup<0>, DrawVoxelCubes);
+type DrawVoxelCubesCommands = (SetMeshViewBindGroup<0>, DrawVoxelCubes);
 
 #[derive(Clone, Component, ExtractComponent)]
 struct VoxelModel;
@@ -537,17 +552,89 @@ where
         pass: &mut TrackedRenderPass<'w>,
     ) -> RenderCommandResult {
         let pipeline = custom_phase_item_buffers.into_inner();
-
-        pass.set_bind_group(1, &pipeline.draw_bind_group, &[]);
         pass.draw_indirect(&pipeline.draw_indirect, 0);
 
         RenderCommandResult::Success
     }
 }
 
+use std::ops::Range;
+
 #[derive(Default)]
 struct VoxelRendererNode {
     ready: bool,
+}
+
+pub struct VoxelBinnedPhaseItem {
+    pub draw_function: DrawFunctionId,
+    // An entity from which data will be fetched, including the mesh if
+    // applicable.
+    pub representative_entity: (Entity, MainEntity),
+    // The ranges of instances.
+    pub batch_range: Range<u32>,
+    // An extra index, which is either a dynamic offset or an index in the
+    // indirect parameters list.
+    pub extra_index: PhaseItemExtraIndex,
+}
+
+impl PhaseItem for VoxelBinnedPhaseItem {
+    #[inline]
+    fn entity(&self) -> Entity {
+        self.representative_entity.0
+    }
+
+    #[inline]
+    fn main_entity(&self) -> MainEntity {
+        self.representative_entity.1
+    }
+
+    #[inline]
+    fn draw_function(&self) -> DrawFunctionId {
+        self.draw_function
+    }
+
+    #[inline]
+    fn batch_range(&self) -> &Range<u32> {
+        &self.batch_range
+    }
+
+    #[inline]
+    fn batch_range_mut(&mut self) -> &mut Range<u32> {
+        &mut self.batch_range
+    }
+
+    fn extra_index(&self) -> PhaseItemExtraIndex {
+        self.extra_index
+    }
+
+    fn batch_range_and_extra_index_mut(&mut self) -> (&mut Range<u32>, &mut PhaseItemExtraIndex) {
+        (&mut self.batch_range, &mut self.extra_index)
+    }
+}
+
+impl BinnedPhaseItem for VoxelBinnedPhaseItem {
+    type BinKey = VoxelBinnedPhaseItemBinKey;
+
+    #[inline]
+    fn new(
+        key: Self::BinKey,
+        representative_entity: (Entity, MainEntity),
+        batch_range: Range<u32>,
+        extra_index: PhaseItemExtraIndex,
+    ) -> Self {
+        Self {
+            draw_function: key.draw_function,
+            representative_entity,
+            batch_range,
+            extra_index,
+        }
+    }
+}
+
+// Data that must be identical in order to batch phase items together.
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct VoxelBinnedPhaseItemBinKey {
+    pub draw_function: DrawFunctionId,
 }
 
 impl render_graph::ViewNode for VoxelRendererNode {
@@ -574,73 +661,141 @@ impl render_graph::ViewNode for VoxelRendererNode {
         }
     }
 
-    type ViewQuery = ();
+    type ViewQuery = (
+        Entity,
+        &'static ExtractedCamera,
+        &'static ViewTarget,
+        &'static ViewDepthTexture,
+        &'static VoxelRenderPipeline,
+        //Option<&'static SkyboxBindGroup>,
+        //&'static ViewUniformOffset,
+    );
 
     // Required method
     fn run<'w>(
         &self,
         graph: &mut RenderGraphContext<'_>,
         render_context: &mut RenderContext<'w>,
-        view_query: <Self::ViewQuery as WorldQuery>::Item<'w>,
+        (
+            view,
+            camera,
+            target,
+            depth,
+            render_pipeline,
+            //skybox_pipeline,
+            //skybox_bind_group,
+            //view_uniform_offset,
+        ): QueryItem<'w, Self::ViewQuery>,
         world: &'w World,
     ) -> Result<(), NodeRunError> {
         if !self.ready {
             return Ok(());
         }
 
+        //let opaque_draw_functions = world.resource::<DrawFunctions<Opaque3d>>();
+        //let draw_custom_phase_item = opaque_draw_functions.read().get::<DrawVoxelCubesCommands>();
+
+        let voxel_phases = world.resource::<ViewBinnedRenderPhases<VoxelBinnedPhaseItem>>();
+        let voxel_phase = voxel_phases.get(&view).unwrap();
+
         let pipeline_cache = world.resource::<PipelineCache>();
         let pipeline = world.resource::<VoxelRendererPipeline>();
 
-        let command_encoder = render_context.command_encoder();
+        let color_attachments = [Some(target.get_color_attachment())];
+        let depth_stencil_attachment = Some(depth.get_attachment(StoreOp::Store));
 
-        command_encoder.clear_buffer(&pipeline.cubes, 0, Some(4));
-        command_encoder.clear_buffer(&pipeline.draw_indirect, 0, Some(4));
+        let view_entity = graph.view_entity();
+        render_context.add_command_buffer_generation_task(move |render_device| {
+            // Command encoder setup
+            let mut command_encoder =
+                render_device.create_command_encoder(&CommandEncoderDescriptor {
+                    label: Some("voxel_command_encoder"),
+                });
 
-        command_encoder.copy_buffer_to_buffer(
-            &pipeline.u32_1_buffer,
-            0,
-            &pipeline.work_items[0].buffer,
-            0,
-            4,
-        );
+            let render_pipeline = match pipeline_cache.get_render_pipeline(render_pipeline.0) {
+                Some(render_pipeline) => render_pipeline,
+                None => return command_encoder.finish(),
+            };
 
-        command_encoder.copy_buffer_to_buffer(
-            &pipeline.first_work_item,
-            0,
-            &pipeline.work_items[0].buffer,
-            // There's padding after the atomic in the buffer as it's not tightly packed.
-            16,
-            std::mem::size_of::<WorkItem>() as u64,
-        );
+            command_encoder.clear_buffer(&pipeline.cubes, 0, Some(4));
+            command_encoder.clear_buffer(&pipeline.draw_indirect, 0, Some(4));
 
-        command_encoder.copy_buffer_to_buffer(
-            &pipeline.u32_1_buffer,
-            0,
-            &pipeline.work_items[0].dispatch,
-            0,
-            std::mem::size_of::<u32>() as u64,
-        );
-
-        let init_pipeline = pipeline_cache
-            .get_compute_pipeline(pipeline.pipeline)
-            .unwrap();
-
-        for (i, uniform_bind_group) in pipeline.uniform_bind_groups.iter().rev().enumerate() {
-            command_encoder.clear_buffer(&pipeline.work_items[(i + 1) % 2].buffer, 0, Some(4));
-            command_encoder.clear_buffer(
-                &pipeline.work_items[(i + 1) % 2].dispatch,
+            command_encoder.copy_buffer_to_buffer(
+                &pipeline.u32_1_buffer,
                 0,
-                Some(std::mem::size_of::<u32>() as u64),
+                &pipeline.work_items[0].buffer,
+                0,
+                4,
             );
 
-            let mut pass = command_encoder.begin_compute_pass(&ComputePassDescriptor::default());
+            command_encoder.copy_buffer_to_buffer(
+                &pipeline.first_work_item,
+                0,
+                &pipeline.work_items[0].buffer,
+                // There's padding after the atomic in the buffer as it's not tightly packed.
+                16,
+                std::mem::size_of::<WorkItem>() as u64,
+            );
 
-            pass.set_bind_group(0, &pipeline.bind_group, &[]);
-            pass.set_bind_group(1, &pipeline.flip_flop_bind_groups[i % 2], &[]);
-            pass.set_bind_group(2, uniform_bind_group, &[]);
-            pass.set_pipeline(init_pipeline);
-            pass.dispatch_workgroups_indirect(&pipeline.work_items[i % 2].dispatch, 0);
-        }
+            command_encoder.copy_buffer_to_buffer(
+                &pipeline.u32_1_buffer,
+                0,
+                &pipeline.work_items[0].dispatch,
+                0,
+                std::mem::size_of::<u32>() as u64,
+            );
+
+            let init_pipeline = pipeline_cache
+                .get_compute_pipeline(pipeline.pipeline)
+                .unwrap();
+
+            for (i, uniform_bind_group) in pipeline.uniform_bind_groups.iter().rev().enumerate() {
+                command_encoder.clear_buffer(&pipeline.work_items[(i + 1) % 2].buffer, 0, Some(4));
+                command_encoder.clear_buffer(
+                    &pipeline.work_items[(i + 1) % 2].dispatch,
+                    0,
+                    Some(std::mem::size_of::<u32>() as u64),
+                );
+
+                let mut pass = command_encoder.begin_compute_pass(&ComputePassDescriptor {
+                    label: Some("voxel_command_encoder_compute_pass"),
+                    ..Default::default()
+                });
+
+                pass.set_pipeline(init_pipeline);
+                pass.set_bind_group(0, &pipeline.bind_group, &[]);
+                pass.set_bind_group(1, &pipeline.flip_flop_bind_groups[i % 2], &[]);
+                pass.set_bind_group(2, uniform_bind_group, &[]);
+                pass.dispatch_workgroups_indirect(&pipeline.work_items[i % 2].dispatch, 0);
+            }
+
+            // Render pass setup
+            let render_pass = command_encoder.begin_render_pass(&RenderPassDescriptor {
+                label: Some("voxel_command_encoder_render_pass"),
+                color_attachments: &color_attachments,
+                depth_stencil_attachment,
+                timestamp_writes: None,
+                occlusion_query_set: None,
+            });
+            let mut render_pass = TrackedRenderPass::new(&render_device, render_pass);
+            //let pass_span = diagnostics.pass_span(&mut render_pass, "main_opaque_pass_3d");
+
+            if let Some(viewport) = camera.viewport.as_ref() {
+                render_pass.set_camera_viewport(viewport);
+            }
+
+            render_pass.set_render_pipeline(render_pipeline);
+            render_pass.set_bind_group(1, &pipeline.draw_bind_group, &[]);
+
+            if let Err(err) = voxel_phase.render(&mut render_pass, world, view_entity) {
+                error!("Error encountered while rendering the opaque phase {err:?}");
+            }
+
+            //pass_span.end(&mut render_pass);
+            drop(render_pass);
+
+            command_encoder.finish()
+        });
 
         Ok(())
     }
