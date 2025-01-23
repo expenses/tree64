31ffdaeceaf8a89f5e3ceeedb518887ed94455fa
Ashley Ruglys <ashley.ruglys@gmail.com>
2025-01-23 08:36:44 +0000

    Fix vox convesion for models with rotations

--- examples/convert_vox.rs
+++ examples/convert_vox.rs
@@ -1,3 +1,7 @@
+fn swizzle(v: glam::IVec3, indices: glam::IVec3) -> glam::IVec3 {
+    glam::IVec3::new(v[indices.x as _], v[indices.y as _], v[indices.z as _])
+}
+
 fn merge_vox_models(vox: dot_vox::DotVoxData) -> (Vec<u8>, glam::UVec3) {
     dbg!(vox.models.len());
 
@@ -11,7 +15,8 @@ fn merge_vox_models(vox: dot_vox::DotVoxData) -> (Vec<u8>, glam::UVec3) {
         _ => panic!(),
     };
 
-    let mut models_and_positions: Vec<(glam::IVec3, glam::UVec3, &dot_vox::Model)> = Vec::new();
+    let mut models_and_positions: Vec<(glam::IVec3, glam::UVec3, glam::IVec3, &dot_vox::Model)> =
+        Vec::new();
 
     for child in children {
         let (child, frames) = match &vox.scenes[*child as usize] {
@@ -19,13 +24,16 @@ fn merge_vox_models(vox: dot_vox::DotVoxData) -> (Vec<u8>, glam::UVec3) {
             _ => panic!(),
         };
 
-        let translation = &frames[0].attributes["_t"];
+        let pos = frames[0]
+            .position()
+            .map(|pos| glam::IVec3::new(pos.x, pos.y, pos.z))
+            .unwrap_or_default();
 
-        let mut splits = translation.split(' ');
-
-        let x = splits.next().unwrap().parse::<i32>().unwrap();
-        let y = splits.next().unwrap().parse::<i32>().unwrap();
-        let z = splits.next().unwrap().parse::<i32>().unwrap();
+        let rotation = frames[0]
+            .orientation()
+            .map(|rot| glam::Mat3::from_cols_array_2d(&rot.to_cols_array_2d()))
+            .unwrap_or_default();
+        let swizzles = (rotation * glam::Vec3::new(0.0, 1.0, 2.0)).as_ivec3();
 
         let model = match &vox.scenes[*child as usize] {
             dot_vox::SceneNode::Shape { models, .. } => models[0].model_id,
@@ -34,15 +42,19 @@ fn merge_vox_models(vox: dot_vox::DotVoxData) -> (Vec<u8>, glam::UVec3) {
 
         let model = &vox.models[model as usize];
 
-        let size = glam::UVec3::new(model.size.x, model.size.y, model.size.z);
+        let size = swizzle(
+            glam::UVec3::new(model.size.x, model.size.y, model.size.z).as_ivec3(),
+            swizzles,
+        )
+        .as_uvec3();
 
-        models_and_positions.push(((x, y, z).into(), size, model));
+        models_and_positions.push((pos, size, swizzles, model));
     }
 
     let mut min = glam::IVec3::splat(i32::MAX);
     let mut max = glam::IVec3::splat(i32::MIN);
 
-    for &(pos, model_size, _) in &models_and_positions {
+    for &(pos, model_size, ..) in &models_and_positions {
         min = min.min(pos - (model_size / 2).as_ivec3());
         max = max.max(pos + (model_size / 2).as_ivec3());
     }
@@ -51,12 +63,15 @@ fn merge_vox_models(vox: dot_vox::DotVoxData) -> (Vec<u8>, glam::UVec3) {
 
     let mut array = vec![0; size.x as usize * size.y as usize * size.z as usize];
 
-    for (pos, model_size, model) in models_and_positions {
+    for (pos, model_size, swizzles, model) in models_and_positions {
         let offset = pos - min - (model_size / 2).as_ivec3();
 
         for voxel in &model.voxels {
-            let voxel_pos =
-                offset + glam::IVec3::new(voxel.x as i32, voxel.y as i32, voxel.z as i32);
+            let voxel_pos = offset
+                + swizzle(
+                    glam::IVec3::new(voxel.x as i32, voxel.y as i32, voxel.z as i32),
+                    swizzles,
+                );
             array[voxel_pos.x as usize
                 + voxel_pos.y as usize * size.x as usize
                 + voxel_pos.z as usize * size.x as usize * size.y as usize] = voxel.i;
